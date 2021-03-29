ceaae3f9036d5cc143aec731d4e2f3031fd450e9
VladimirYugay <voviktyl@gmail.com>
2021-03-29 10:20:24 +0200

    Import variable loading function from main module in the inference script

--- scripts/run_depth_inference.py
+++ scripts/run_depth_inference.py
@@ -7,13 +7,12 @@ from pathlib import Path
 import click
 import matplotlib.pyplot as plt
 import numpy as np
-import tensorflow
 import tensorflow.compat.v1 as tf
 from IPython.core import ultratb
 from PIL import Image
 
 import diw
-from diw.model import Model
+from diw.model import Model, get_vars_to_save_and_restore
 
 sys.excepthook = ultratb.FormattedTB(mode="Verbose", color_scheme="Linux", call_pdb=1)
 
@@ -39,69 +38,6 @@ def plot_image(image, image_type="RGB"):
     plt.imshow(image, cmap=color_map)
     plt.show()  # display it
     return plt
-
-
-def get_vars_to_save_and_restore(ckpt=None):
-    """Returns list of variables that should be saved/restored.
-    Args:
-      ckpt: Path to existing checkpoint.  If present, returns only the subset of
-          variables that exist in given checkpoint.
-    Returns:
-      List of all variables that need to be saved/restored.
-    """
-    model_vars = tf.trainable_variables()
-    # Add batchnorm variables.
-    bn_vars = [
-        v
-        for v in tf.global_variables()
-        if "moving_mean" in v.op.name
-        or "moving_variance" in v.op.name
-        or "mu" in v.op.name
-        or "sigma" in v.op.name
-        or "global_scale_var" in v.op.name
-    ]
-    model_vars.extend(bn_vars)
-    model_vars = sorted(model_vars, key=lambda x: x.op.name)
-    mapping = {}
-    if ckpt is not None:
-        ckpt_var = tensorflow.train.list_variables(ckpt)
-        ckpt_var_names = [name for (name, unused_shape) in ckpt_var]
-        ckpt_var_shapes = [shape for (unused_name, shape) in ckpt_var]
-        not_loaded = list(ckpt_var_names)
-        for v in model_vars:
-            if v.op.name not in ckpt_var_names:
-                # For backward compatibility, try additional matching.
-                v_additional_name = v.op.name.replace("egomotion_prediction/", "")
-                if v_additional_name in ckpt_var_names:
-                    # Check if shapes match.
-                    ind = ckpt_var_names.index(v_additional_name)
-                    if ckpt_var_shapes[ind] == v.get_shape():
-                        mapping[v_additional_name] = v
-                        not_loaded.remove(v_additional_name)
-                        continue
-                    else:
-                        logging.warn("Shape mismatch, will not restore %s.", v.op.name)
-                logging.warn(
-                    "Did not find var %s in checkpoint: %s",
-                    v.op.name,
-                    os.path.basename(ckpt),
-                )
-            else:
-                # Check if shapes match.
-                ind = ckpt_var_names.index(v.op.name)
-                if ckpt_var_shapes[ind] == v.get_shape() and v.op.name in not_loaded:
-                    mapping[v.op.name] = v
-                    not_loaded.remove(v.op.name)
-                else:
-                    logging.warn("Shape mismatch, will not restore %s.", v.op.name)
-        if not_loaded:
-            logging.warn("The following variables in the checkpoint were not loaded:")
-            for varname_not_loaded in not_loaded:
-                logging.info("%s", varname_not_loaded)
-    else:  # just get model vars.
-        for v in model_vars:
-            mapping[v.op.name] = v
-    return mapping
 
 
 @click.command()
